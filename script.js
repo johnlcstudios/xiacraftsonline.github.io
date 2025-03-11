e2664b1ce5b986c50cb7f9a90af4f9a148654f45
johnlcstudios <jlcabajaan@gmail.com>
2025-03-11 23:27:39 +0800

    added navigation

--- script.js
+++ script.js
@@ -101,3 +101,9 @@ document.addEventListener('DOMContentLoaded', function() {
     displayProducts();
     displayRecommendedProducts();
 });
+
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide links
+});
